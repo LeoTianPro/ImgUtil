277910c71c80fe39493ddc3c56203f8d764b946b
youre <465858129@qq.com>
2018-06-08 20:03:57 +0800

    No change.

--- view/ImagePanel.java
+++ view/ImagePanel.java
@@ -1,58 +1,52 @@
 /**
- *
- * @author  Titanlbr520
+ * @author Titanlbr520
  * @function 比例缩放
  */
 package view;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-
-import javax.swing.JComponent;
+import javax.swing.*;
+import java.awt.*;
 
 public class ImagePanel extends JComponent {
     protected float scale = 1;          // 缩放比例
     protected Image image = null;       // 需要显示的图像
-    
+
     public ImagePanel(Image image) {
         setImage(image);
     }
-    
+
     public void setImage(Image image) {
         this.image = image;
         setSize(getPreferredSize());
     }
-    
+
     /** 设置缩放比例*/
     public void setScale(float scale) {
         this.scale = scale;
         setSize(getPreferredSize());
     }
-    
+
     /** 获取需要显示图像缩放后的尺寸*/
     protected Dimension getImageSize() {
 
 
-
-        if(image != null) {
-            return new Dimension(Math.round(image.getWidth(null)*scale), Math.round(image.getHeight(null)*scale));
-        }
-        else return new Dimension(0, 0);
+        if (image != null) {
+            return new Dimension(Math.round(image.getWidth(null) * scale), Math.round(image.getHeight(null) * scale));
+        } else return new Dimension(0, 0);
     }
-    
+
     /** 获取控件首选尺寸，由于没有边框，它等于getImageSize()*/
     public Dimension getPreferredSize() {
-        return getImageSize();    
+        return getImageSize();
     }
-    
-    
+
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        if(image == null) return ;
+
+        if (image == null) return;
         Dimension destDim = getImageSize();
         g.drawImage(image, 0, 0, destDim.width, destDim.height,
-                    0, 0, image.getWidth(null), image.getHeight(null), null);
+                0, 0, image.getWidth(null), image.getHeight(null), null);
     }
 }
